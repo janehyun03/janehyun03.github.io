844314a0659196b46527dbe6ef95ee3198adc8ee
janehyun03 <janehyun03@gmail.com>
2024-07-10 18:53:01 -0400

    omni-food edited

---
+++ script2.js
@@ -0,0 +1,9 @@
+$('.skills-images').slick({
+  dots: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  // autoplay: true,
+  // autoplaySpeed: 2000,
+  nextArrow:$('.next'),
+  prevArrow:$('.prev'),
+});
