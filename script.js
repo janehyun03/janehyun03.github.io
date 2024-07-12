d0e039d8c52a0bd3d548101eaa6a333accb289bf
janehyun03 <janehyun03@gmail.com>
2024-07-12 19:24:27 -0400

    about me slide dots added

--- script.js
+++ script.js
@@ -37,17 +37,44 @@ $(document).ready(function () {
     infinite: true,
     autoplay: true,
     autoplaySpeed: 2000,
-    speed: 1000,
+    speed: 1000,	
+    fade: true,
+    fadeSpeed: 1000,
     slidesToShow:1,
     slidesToScroll: 1,
     arrows:false,
-    // dots: true,
+    dots:true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   });
-});
 
 const typed = new Typed(".animate", {
   strings: ["Jane.", "a lecturer.", "a travaler.", "a developer."],
   typeSpeed: 100,
   backSpeed: 100,
   loop: true,
+});
+
 });
